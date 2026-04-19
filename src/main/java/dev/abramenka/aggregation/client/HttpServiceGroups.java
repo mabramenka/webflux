a3481549e07662d1ecd103753878ba2fc5df7b00
Mikita <mikita.by@gmail.com>
2026-04-19 23:40:36 +0200

    refactor: simplify HttpServiceGroups

--- src/main/java/dev/abramenka/aggregation/client/HttpServiceGroups.java
+++ src/main/java/dev/abramenka/aggregation/client/HttpServiceGroups.java
@@ -1,17 +1,17 @@
 package dev.abramenka.aggregation.client;
 
 import java.util.Arrays;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public final class HttpServiceGroups {
 
     public static final String ACCOUNT_GROUP = "account-group";
     public static final String ACCOUNT = "account";
     public static final String OWNERS = "owners";
 
-    private HttpServiceGroups() {}
-
     public static String downstreamClientName(String groupName) {
-        return Group.fromId(groupName).displayName(groupName);
+        return Group.fromId(groupName).displayName;
     }
 
     private enum Group {
@@ -33,10 +33,6 @@ public final class HttpServiceGroups {
                     .filter(group -> group.id.equals(id))
                     .findFirst()
                     .orElse(UNKNOWN);
-        }
-
-        String displayName(String fallback) {
-            return this == UNKNOWN ? fallback : displayName;
         }
     }
 }
