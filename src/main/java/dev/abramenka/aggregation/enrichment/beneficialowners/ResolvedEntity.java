4c62d3db4ed02c43f2e7b103f5ec8fcd8e13ba39
Mikita <mikita.by@gmail.com>
2026-04-28 18:30:16 +0200

    refactor(workflow): remove legacy beneficial owners helpers

--- src/main/java/dev/abramenka/aggregation/enrichment/beneficialowners/ResolvedEntity.java
+++
@@ -1,5 +0,0 @@
-package dev.abramenka.aggregation.enrichment.beneficialowners;
-
-import tools.jackson.databind.node.ArrayNode;
-
-record ResolvedEntity(int dataIndex, int ownerIndex, ArrayNode details) {}
